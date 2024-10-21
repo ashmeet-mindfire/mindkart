750f5bd8d30009de2152fa2d16b5ac1f9d9e0928
ashmeet-mindfire <mfsi.ashmeetm@gmail.com>
2024-10-21 16:19:14 +0530

    authorize admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_only
   def index
+  end
+
+  private
+  def admin_only
+    unless current_user.admin?
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    end
   end
 end
