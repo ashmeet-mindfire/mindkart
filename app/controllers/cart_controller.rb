c72ebbf01b1d1491c4e031e594019bd32a9e7c66
ashmeet-mindfire <mfsi.ashmeetm@gmail.com>
2024-10-23 18:45:31 +0530

    cart controller minor edits

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -9,15 +9,12 @@ class CartController < ApplicationController
     @cart = current_user.cart || Cart.create(user: current_user)
     book = Book.find(params[:book_id])
 
-    # Check if the item already exists in the cart
     cart_item = @cart.cart_items.find_by(book_id: book.id)
 
     if cart_item
-      # Increase the quantity if the item already exists
       cart_item.quantity += 1
       cart_item.save
     else
-      # Create a new cart item if it doesn't exist
       @cart.cart_items.create(book: book, quantity: 1)
     end
 
