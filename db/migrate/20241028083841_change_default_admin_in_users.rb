b8cb8f9343477d5850fea5c4ef0ae86cb88bcac4
ashmeet-mindfire <mfsi.ashmeetm@gmail.com>
2024-10-28 14:09:08 +0530

    Change admin default to false

--- db/migrate/20241028083841_change_default_admin_in_users.rb
+++ db/migrate/20241028083841_change_default_admin_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultAdminInUsers < ActiveRecord::Migration[7.2]
   def change
-    change_column_default :users, :admin, true
+    change_column_default :users, :admin, false
   end
 end
